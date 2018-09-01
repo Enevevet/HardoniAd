b48bb5ccecf5c904e080aef56dd0bbef35b15300
Enevevet <moi.201214@hotmail.fr>
2018-09-01 18:19:56 +0200

    Update du baninfo.js pour le sans args

--- commands/moderation/baninfo.js
+++ commands/moderation/baninfo.js
@@ -3,10 +3,12 @@ exports.run = (client, message, args) => {
 	//On définit le chenapan
 	var user;
 	message.mentions.users.first() ? user = message.mentions.users.first() : user = client.users.get(args[0]);
-	if (!user) return message.channel.send(`Merci de rentrer un utilisateur valide.`);
+	if (!user) user = message.author;
 	var ui = user.id;
+
 	//S'il n'est pas ban on rekt
-	if (!client.banned.has(ui)) return message.channel.send(`Celui-ci n'est pas ban`);
+	if (!client.banned.has(ui) && user === message.author) return message.channel.send(`${client.em.d} Tu n'es pas banni !`);
+	else if (!client.banned.has(ui)) return message.channel.send(`${client.em.e} Cet utilisateur n'est pas ban`);
 
 	//Sinon on chope les infos
 	let r = client.banned.getProp(ui, `reason`);
